bfbb6288f294962c25164c9fce0e687fad6605c5
Satyanarayan Rao <satyanarayan.iiitm@gmail.com>
2022-01-26 20:45:24 -0700

    added ghostscript and removed plots

--- install_required_packages.sh
+++ install_required_packages.sh
@@ -1,3 +1,3 @@
 conda install -c bioconda snakemake trim-galore=0.6.2 bedtools bowtie2 bismark samtools=1.9 pyBigWig=0.3.14 bamtools=2.4.1
 conda install pandas=0.25.3 numpy=1.16.3 tbb=2020.2
-conda install -c conda-forge gnuplot
+conda install -c conda-forge gnuplot ghostscript
